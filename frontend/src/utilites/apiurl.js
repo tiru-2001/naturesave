0bc70bb152a7af4aa11d317fa97d1001465f89aa
tiru-2001 <tirumaleshae709@gmail.com>
2024-07-16 14:11:11 +0530

    changed vercel  url

--- frontend/src/utilites/apiurl.js
+++ frontend/src/utilites/apiurl.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://naturesave.vercel.app/' });
+const API = axios.create({ baseURL: 'https://naturesave.vercel.app/api/v1/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
